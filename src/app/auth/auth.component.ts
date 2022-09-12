62912c5da68d229c98a9765555adb16f5508c39b
ThomasCode92 <thomascode92@hotmail.com>
2022-09-12 13:29:21 +0200

    Using ngIf

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -21,6 +21,10 @@ export class AuthComponent {
     this.isLoginMode = !this.isLoginMode;
   }
 
+  onHandleError() {
+    this.error = null;
+  }
+
   onSubmit(form: NgForm) {
     if (!form.valid) return;
 
@@ -39,7 +43,7 @@ export class AuthComponent {
 
     authObs.subscribe({
       next: responseData => {
-        console.log(responseData);        
+        console.log(responseData);
         this.isLoading = false;
         this.router.navigate(['/recipes']);
       },

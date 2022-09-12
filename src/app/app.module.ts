223a6c96a6854fca57999d67350901c51787f5c8
ThomasCode92 <thomascode92@hotmail.com>
2022-09-12 13:22:32 +0200

    Adding an Alert Modal Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { AlertComponent } from './shared/alert/alert.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 
 import { AuthInterceptor } from './auth/auth.interceptor';
@@ -38,6 +39,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AlertComponent,
     LoadingSpinnerComponent,
   ],
   imports: [

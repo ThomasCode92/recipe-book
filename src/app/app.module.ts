0bb43197c80c0d75fd97573637831cabeac774c0
ThomasCode92 <thomascode92@hotmail.com>
2022-09-14 13:05:47 +0200

    Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,26 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
+import { CoreModule } from './core.module';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
-import { SharedModule } from './shared/shared.module';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
-import { CoreModule } from './core.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, AuthComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     CoreModule,
     SharedModule,
     AppRoutingModule,
+    AuthModule,
     RecipesModule,
     ShoppingListModule,
   ],

b8393665e11f15d385026febb38ade2ae5e6f63a
ThomasCode92 <thomascode92@hotmail.com>
2022-09-19 08:05:42 +0200

    Preparing Update & Delete Actions

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -4,6 +4,8 @@ import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = '[Shopping List] Add Ingredient';
 export const ADD_INGREDIENTS = '[Shopping List] Add Ingredients';
+export const UPDATE_INGREDIENT = '[Shopping List] Update Ingredient';
+export const DELETE_INGREDIENT = '[Shopping List] Delete Ingredient';
 
 export const addIngredient = createAction(
   ADD_INGREDIENT,
@@ -13,4 +15,14 @@ export const addIngredient = createAction(
 export const addIngredients = createAction(
   ADD_INGREDIENTS,
   props<{ ingredients: Ingredient[] }>()
+);
+
+export const updateIngredient = createAction(
+  UPDATE_INGREDIENT,
+  props<{ index: number; ingredient: Ingredient }>()
+);
+
+export const deleteIngredient = createAction(
+  DELETE_INGREDIENT,
+  props<{ index: number }>()
 );

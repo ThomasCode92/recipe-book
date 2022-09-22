97701626389361c07c242d1aa77d84173ab33ffb
ThomasCode92 <thomascode92@hotmail.com>
2022-09-22 21:29:52 +0200

    Using the Store Devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +15,8 @@ import { HeaderComponent } from './header/header.component';
 import { appReducer } from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
   imports: [
@@ -21,6 +24,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     HttpClientModule,
     StoreModule.forRoot(appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
     CoreModule,
     SharedModule,
     AppRoutingModule,

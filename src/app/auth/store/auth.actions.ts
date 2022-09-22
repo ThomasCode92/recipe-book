d2cf9e4fb78e31103a0b77ea8984353487c99f44
ThomasCode92 <thomascode92@hotmail.com>
2022-09-22 18:56:18 +0200

    Effects & Error Handling

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -4,6 +4,11 @@ import { User } from '../user.model';
 
 export const LOGIN_START = '[Auth] Login start';
 
+export const loginStart = createAction(
+  LOGIN_START,
+  props<{ email: string; password: string }>()
+);
+
 export const login = createAction('[Auth] Login', props<{ user: User }>());
 
 export const logout = createAction('[Auth] Logout');
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,11 +1,48 @@
+import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, of, switchMap } from 'rxjs';
 
 import { LOGIN_START } from './auth.actions';
 
-export class AuthEffects {
-  authLogin = this.actions$.pipe(
-    ofType(LOGIN_START)
-  );
+import { environment } from '../../../environments/environment';
 
-  constructor(private actions$: Actions) {}
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
+}
+
+const FIREBASE_API_KEY = environment.firebaseAPIKey;
+
+export class AuthEffects {
+  authLogin = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(LOGIN_START),
+      switchMap(authData => {
+        return this.http
+          .post<AuthResponseData>(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`,
+            {
+              email: authData.email,
+              password: authData.password,
+              returnSecureToken: true,
+            }
+          )
+          .pipe(
+            catchError(error => {
+              // ...
+              of();
+            }),
+            map(responseData => {
+              of();
+            })
+          );
+      })
+    );
+  });
+
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
